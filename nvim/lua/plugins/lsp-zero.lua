5e1b65765c06fd63d2afdd2e4d081eb1f8c4f284
Kauan Carvalho Calasans <kauan_calasans@hotmail.com>
2023-09-20 14:29:03 -0300

    Improve LSP general

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -1,43 +1,58 @@
 return {
-  {'hrsh7th/nvim-cmp'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'neovim/nvim-lspconfig'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
+    lazy = true,
+    config = function()
+      require('lsp-zero.settings').preset({})
+    end
+  },
+
+  -- Autocompletion.
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
-      -- LSP Support.
-      {'neovim/nvim-lspconfig'},             -- Required.
-      {'williamboman/mason.nvim'},           -- Optional.
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional.
-
-      -- Autocompletion.
-      {'hrsh7th/nvim-cmp'},                  -- Required.
-      {'hrsh7th/cmp-nvim-lsp'},              -- Required.
-
-      -- Snippets.
-      {'L3MON4D3/LuaSnip'},                  -- Required.
+      {
+        'L3MON4D3/LuaSnip'
+      }
     },
     config = function()
-      local lsp = require('lsp-zero').preset({
-          name = 'minimal',
-          set_lsp_keymaps = true,
-          manage_nvim_cmp = true,
-          suggest_lsp_servers = false,
-        })
+      require('lsp-zero.cmp').extend()
 
-      -- Configure lua language server for neovim. Optiona.l
-      lsp.nvim_workspace()
+      local cmp = require('cmp')
 
-      -- Don't preselect first match.
-      lsp.setup_nvim_cmp({
-        preselect = 'none',
-        completion = {
-          completeopt = 'menu,menuone,noinsert,noselect'
+      cmp.setup({
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = false })
         },
+        sources = {
+          { name = 'nvim_lsp' }
+        }
       })
+    end
+  },
+
+  -- LSP.
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'williamboman/mason.nvim'},
+    },
+    config = function()
+      local lsp = require('lsp-zero')
+
+      lsp.on_attach(function(client, bufnr)
+        -- See :help lsp-zero-keybindings to learn the available actions.
+        lsp.default_keymaps({buffer = bufnr})
+      end)
+
+      -- Configure lua language server for neovim.
+      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
       lsp.setup()
     end
