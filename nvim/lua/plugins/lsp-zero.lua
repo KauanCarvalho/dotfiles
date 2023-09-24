91dbf5069b92aa76d38dec07676892bedfbaf6e4
Kauan Carvalho <kauan.carvalho.calasans@gmail.com>
2023-09-24 00:14:16 -0300

    Wip - Adds ts lsp support for nvm

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -54,6 +54,9 @@ return {
       -- Configure lua language server for neovim.
       require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+      -- Configure ts language server for neovim.
+      require('lspconfig').tsserver.setup({})
+
       lsp.setup()
     end
   }
